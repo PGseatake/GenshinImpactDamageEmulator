6e86b90d2bf9abfc62b375dbe915edcd16c8279b
PGseatake <37163168+PGseatake@users.noreply.github.com>
2021-05-02 21:35:22 +0900

    文字列削除時の空白対応

--- components/VNumberField.vue
+++ components/VNumberField.vue
@@ -2,10 +2,11 @@
   <v-text-field
     v-bind="$attrs"
     v-on="$listeners"
-    v-model.number="value"
+    v-model="value"
     type="number"
     min="0"
     :step="step"
+    :rules="[rule]"
   >
     <template v-for="(item, name) in $slots" v-slot:[name]>
       <slot :name="name" />
@@ -35,13 +36,24 @@ export default Vue.extend({
       default: 1,
     },
   },
+  data() {
+    return {
+      // TODO: 値削除時に自動で有効な値に変える
+      rule: (value: string) => !!value || "Input number.",
+    };
+  },
   computed: {
     value: {
-      get(): number {
-        return this.refer.value;
+      get(): string {
+        return this.refer.value.toString();
       },
-      set(value: number) {
-        this.refer.value = value;
+      set(value: string) {
+        if (value) {
+          this.refer.value = parseFloat(value);
+        } else {
+          this.refer.value = 0;
+          // TODO: 一度は0に変わるが2回削除すると空白になってしまう
+        }
       },
     },
     step: {
